098efb6f865976abcef8658b8cef8aadb82318b9
nnorius <nnorius@iastate.edu>
2024-02-17 20:38:05 -0600

    added run button, upload button, different webpage tabs, and styling

--- basic_webpage/index.js
+++ basic_webpage/index.js
@@ -1,3 +1,4 @@
+
 const csvUrl = 'http://localhost:8080/sample5062_input.csv'; 
 
 async function loadModel(){
@@ -26,5 +27,51 @@ async function runModel(){
     prediction = model.predict(tensor);
     const result = prediction.dataSync();
     console.log(result[0]);
+
+    document.getElementById('total').innerHTML = result[0];
+
+
 }
-window.onload = runModel;
+
+function selectFile(){
+    var input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = e => { 
+        var file = e.target.files[0]; 
+    }
+
+    input.click();
+}
+
+
+window.onload = load;
+
+function load(){
+    const elem = document.getElementById("defaultOpen");
+    elem.click();
+
+}
+
+
+function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = 'rgb('+139+','+ 156+','+ 158+')'; 
+  }
+  
