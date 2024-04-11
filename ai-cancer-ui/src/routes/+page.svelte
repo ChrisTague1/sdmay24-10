ea637c5dd3292da7f9e12ad9bf8dd3ae0f99fd1a
Mark Hanson <mphanson@iastate.edu>
2024-04-10 20:01:35 -0500

    finished login functionality

--- ai-cancer-ui/src/routes/+page.svelte
+++ ai-cancer-ui/src/routes/+page.svelte
@@ -1,63 +1,105 @@
+
 <script>
-    /*// Import the functions you need from the SDKs you need
-    import { initializeApp } from "firebase/app";
-    import { getAnalytics } from "firebase/analytics";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-        apiKey: "AIzaSyCRArYuRx4hOIEntpd6sOj7PnwOzMG29A0",
-        authDomain: "sdmay24-10.firebaseapp.com",
-        projectId: "sdmay24-10",
-        storageBucket: "sdmay24-10.appspot.com",
-        messagingSenderId: "159889844134",
-        appId: "1:159889844134:web:f5f20aa862754ba2b29167",
-        measurementId: "G-E2BZSNX333"
-    };
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);*/
-
-
-
-
-
-
     import { goto } from "$app/navigation";
-    $: submit = async () =>
-    {
-        // @ts-ignore
-        if(document.getElementById("username").value==="admin"&&document.getElementById("password").value==="admin")
+	import { authHandlers, authStore } from '../stores/authStore';
+
+	let register = false;
+	let email = '';
+	let password = '';
+	let confirmPassword = '';
+
+	async function handleSubmit() {
+		if (!email || !password || (register && !confirmPassword)) {
+			return;
+		}
+		if (register && password === confirmPassword) {
+			try {
+				await authHandlers.signup(email, password);
+                goto('/');
+			} catch (err) {
+				console.log(err);
+                // @ts-ignore
+                console.log(err.message);
+                // @ts-ignore
+                if(err.message=="Firebase: Error (auth/email-already-in-use).")
+                {
+                    // @ts-ignore
+                    document.getElementById("ErrMsg").innerHTML = "Email already in use"
+                }
+                // @ts-ignore
+                if(err.message=="Firebase: Error (auth/invalid-email).")
+                {
+                    // @ts-ignore
+                    document.getElementById("ErrMsg").innerHTML = "Invalid email"
+                }
+                // @ts-ignore
+                if(err.message=="Firebase: Password should be at least 6 characters (auth/weak-password).")
+                {
+                    // @ts-ignore
+                    document.getElementById("ErrMsg").innerHTML = "Password must be at least 6 characters"
+                }
+			}
+		} 
+        else if(register && password != confirmPassword)
         {
-            goto('/app')
+            // @ts-ignore
+            document.getElementById("ErrMsg").innerHTML = "Passwords do not match"
         }
         else
         {
-
-        }
-    }
-    function create()
-    {
-        console.log("HELLO")
-        goto('/create')
-    }
+			try {
+				await authHandlers.login(email, password);
+                goto('/app');
+			} catch (err) {
+				console.log(err);
+                // @ts-ignore
+                document.getElementById("ErrMsg").innerHTML = "Incorrect Email or Password"
+			}
+		}
+	}
 </script>
 
-<main class="form-signin">
-    <form on:submit|preventDefault={submit}>
-        <div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-xl">
-            <h1 class="text-2xl font-bold mb-4">Please Sign In</h1>
-            <div class="form-floating">
-                <label class="text-gray-600 mb-4">Username</label>
-                <input id="username" class="form-control" placeholder="username">
-            </div>
-            <div class="form-floating">
-                <label class="text-gray-600 mb-4">Password</label>
-                <input type="password" id="password" class="form-control" placeholder="password">
-            </div>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
-            <p id="CreateAccount" class="text-blue-600 mb-4" on:click={create}>Create Account</p>
-        </div>
-    </form>
-</main>
+<div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-xl">
+	<h1 class="text-2xl font-bold mb-4">{register ? 'Register' : 'Log in'}</h1>
+	<form>
+		<label class="text-gray-600 mb-4">
+			<input bind:value={email} type="email" placeholder="Email" />
+		</label>
+        <div></div>
+		<label class="text-gray-600 mb-4">
+			<input bind:value={password} type="password" placeholder="Password" />
+		</label>
+        <div></div>
+		{#if register}
+			<label class="text-gray-600 mb-4">
+				<input bind:value={confirmPassword} type="password" placeholder="Confirm Password" />
+			</label>
+		{/if}
+        <div style="color: red;font-size: medium;" id="ErrMsg"></div>
+		<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={handleSubmit}>Submit</button>
+	</form>
+	{#if register}
+		<div
+			on:click={() => {
+                // @ts-ignore
+                document.getElementById("ErrMsg").innerHTML = ""
+				register = false;
+			}}
+			on:keydown={() => {}}
+		>
+			Already have an account?
+			<p style="color: blue;font-size: medium;">Log in</p>
+		</div>
+	{:else}
+		<div
+			on:click={() => {
+                // @ts-ignore
+                document.getElementById("ErrMsg").innerHTML = ""
+				register = true;
+			}}
+			on:keydown={() => {}}
+		>
+			Don't have an account? <p style="color: blue;font-size: medium;">Sign Up</p>
+		</div>
+	{/if}
+</div>
