714350777ffd1f4805e0e6f84349121caae0b30e
Chris Tague <cstague6401@gmail.com>
2024-04-24 23:05:49 -0500

    dashboard changes

--- ai-cancer-ui/src/routes/(app)/dashboard/+page.svelte
+++ ai-cancer-ui/src/routes/(app)/dashboard/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-    import { goto } from "$app/navigation";
-    import type { PageData } from "./$types";
-
-    export let data: PageData;
-
-    let { supabase } = data;
-    $: ({ supabase } = data);
-</script>
 Dashboard page
-<button
-    on:click={async () => {
-        await supabase.auth.signOut();
-        goto('/auth')
-    }}
->
-    Sign out
-</button>
